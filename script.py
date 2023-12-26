2871bf054ce2e70a3276e5783d45dc3ecbf3cade
Adam Månsson <adam.s.mansson@gmail.com>
2023-12-26 13:44:07 +0100

    Improve verbose output

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-import argparse
+from argparse import ArgumentParser
 from pypdf import PdfReader
 from datetime import date, datetime
 from dataclasses import dataclass
+from sty import fg, bg, ef, rs
 
 DATE_FORMAT = '%Y-%m-%d'
 
@@ -84,7 +85,7 @@ def sorted_courses(courses, sort_by):
         case _:
             raise ValueError
 
-    return sorted(courses, key = sort_by_key)
+    return sorted(courses, key=sort_by_key)
 
 def get_lines(file_name):
     reader = PdfReader(file_name)
@@ -99,18 +100,48 @@ def processed_courses(courses, sort_by, include_ug):
         courses = list(filter(lambda course : course.grade != 'G', courses))
     return sorted_courses(courses, sort_by)
 
-def print_course(course):
-    repr = ' '.join([course.name, str(course.scope) + 'hp', str(course.grade), course.date.strftime(DATE_FORMAT)])
-    print(repr)
+DARK_COL_START = bg.grey + fg.black
+DARK_COL_END = fg.rs + bg.rs
+LIGHT_COL_START = bg.li_grey + fg.black
+LIGHT_COL_END = fg.rs + bg.rs
+HEAD_START = ef.u + ef.bold + DARK_COL_START
+HEAD_END = rs.bold_dim + rs.u
+
+def print_course(course, name_width, scope_width, grade_width, date_width):
+    name = DARK_COL_START + course.name.ljust(name_width, ' ') + DARK_COL_END
+    scope = LIGHT_COL_START + (str(course.scope) + 'hp').center(scope_width, ' ') + LIGHT_COL_END
+    grade = DARK_COL_START + (str(course.grade) + '  ').center(grade_width, ' ') + DARK_COL_END
+    date = LIGHT_COL_START + course.date.strftime(DATE_FORMAT).center(date_width, ' ') + LIGHT_COL_END
+    print(name + scope + grade + date)
 
 def print_courses(header, courses):
+    header = header.split(" ")
+
+    name = header[0]
+    name_width = max(list(map(lambda course : len(course.name), courses)))
+    name_width = max(name_width + 1, len(name))
+    name = HEAD_START + DARK_COL_START + name.ljust(name_width, ' ') + DARK_COL_END + HEAD_END
+
+    scope = header[1]
+    scope_width = max(5, len(scope) + 2)
+    scope = HEAD_START + LIGHT_COL_START + scope.center(scope_width, ' ') + LIGHT_COL_END + HEAD_END
+
+    grade = header[2]
+    grade_width = max(3, len(grade) + 2)
+    grade = HEAD_START + DARK_COL_START + grade.center(grade_width, ' ') + DARK_COL_END + HEAD_END
+
+    date = header[3]
+    date_width = max(10, len(date))
+    date = HEAD_START + LIGHT_COL_START + date.center(date_width, ' ') + LIGHT_COL_END + HEAD_END
+
+    header = name + scope + grade + date
     print(header)
     for course in courses:
-        print_course(course)
+        print_course(course, name_width, scope_width, grade_width, date_width)
     print()
  
 def main():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
                     prog='ladok-average',
                     description='Calculate your grade average from Ladok.')
 
