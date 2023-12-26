162c1d1a5849d01fd1507d6c1628efcd949057bf
Adam Månsson <adam.s.mansson@gmail.com>
2023-12-26 14:13:10 +0100

    Refactor printing of header

--- script.py
+++ script.py
@@ -107,6 +107,13 @@ LIGHT_COL_END = fg.rs + bg.rs
 HEAD_START = ef.u + ef.bold + DARK_COL_START
 HEAD_END = rs.bold_dim + rs.u
 
+def print_header(header, name_width, scope_width, grade_width, date_width):
+    name = HEAD_START + DARK_COL_START + header[0].ljust(name_width, ' ') + DARK_COL_END + HEAD_END
+    scope = HEAD_START + LIGHT_COL_START + header[1].center(scope_width, ' ') + LIGHT_COL_END + HEAD_END
+    grade = HEAD_START + DARK_COL_START + header[2].center(grade_width, ' ') + DARK_COL_END + HEAD_END
+    date = HEAD_START + LIGHT_COL_START + header[3].center(date_width, ' ') + LIGHT_COL_END + HEAD_END
+    print(name + scope + grade + date)
+
 def print_course(course, name_width, scope_width, grade_width, date_width):
     name = DARK_COL_START + course.name.ljust(name_width, ' ') + DARK_COL_END
     scope = LIGHT_COL_START + (str(course.scope) + 'hp').center(scope_width, ' ') + LIGHT_COL_END
@@ -117,25 +124,13 @@ def print_course(course, name_width, scope_width, grade_width, date_width):
 def print_courses(header, courses):
     header = header.split(" ")
 
-    name = header[0]
     name_width = max(list(map(lambda course : len(course.name), courses)))
-    name_width = max(name_width + 1, len(name))
-    name = HEAD_START + DARK_COL_START + name.ljust(name_width, ' ') + DARK_COL_END + HEAD_END
+    name_width = max(name_width + 1, len(header[0]))
+    scope_width = max(5, len(header[1]) + 2)
+    grade_width = max(3, len(header[2]) + 2)
+    date_width = max(10, len(header[3]))
 
-    scope = header[1]
-    scope_width = max(5, len(scope) + 2)
-    scope = HEAD_START + LIGHT_COL_START + scope.center(scope_width, ' ') + LIGHT_COL_END + HEAD_END
-
-    grade = header[2]
-    grade_width = max(3, len(grade) + 2)
-    grade = HEAD_START + DARK_COL_START + grade.center(grade_width, ' ') + DARK_COL_END + HEAD_END
-
-    date = header[3]
-    date_width = max(10, len(date))
-    date = HEAD_START + LIGHT_COL_START + date.center(date_width, ' ') + LIGHT_COL_END + HEAD_END
-
-    header = name + scope + grade + date
-    print(header)
+    print_header(header, name_width, scope_width, grade_width, date_width)
     for course in courses:
         print_course(course, name_width, scope_width, grade_width, date_width)
     print()
